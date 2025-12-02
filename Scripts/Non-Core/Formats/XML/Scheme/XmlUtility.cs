54a149089590f9e27e2f9a302a9598db8319a3bb
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-12-02 23:41:42 +0100

    Fixed InnerXml call in XmlUtility, replaced with InnerText.

--- Scripts/Non-Core/Formats/XML/Scheme/XmlUtility.cs
+++ Scripts/Non-Core/Formats/XML/Scheme/XmlUtility.cs
@@ -31,24 +31,8 @@ namespace Rusty.Serialization.Serializers.XML
         /// </summary>
         public static XmlElement Pack(string innerText, string name, params (string, string)[] attributePairs)
         {
-            /*StringBuilder sb = new();
-            for (int i = 0; i < innerText.Length; i++)
-            {
-                char c = innerText[i];
-
-                if (c == 0x9 || c == 0xA || c == 0xD ||
-                    (c >= 0x20 && c <= 0xD7FF) ||
-                    (c >= 0xE000 && c <= 0xFFFD) ||
-                    (c >= 0x10000 && c <= 0x10FFFF))
-                {
-                    sb.Append(c);
-                }
-                else
-                    sb.Append($"\\[{c}]");
-            }*/
-
             XmlElement element = Create(name);
-            element.InnerXml = innerText.ToString();
+            element.InnerText = innerText.ToString();
             return element;
         }
 
