594d1a9317a1710aea4b9b53389db3aff891afee
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-12-16 14:43:32 +0100

    New nullable converter.

--- Scripts/Core/Converters/Contexts/CreateNodeContext.cs
+++ Scripts/Core/Converters/Contexts/CreateNodeContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Rusty.Serialization.Core.Converters
+﻿using Rusty.Serialization.Core.Nodes;
+
+namespace Rusty.Serialization.Core.Converters
 {
     public class CreateNodeContext
     {
+        /* Public methods. */
+        public INode CreateNode(object obj)
+        {
+            return null;
+        }
     }
 }
---
+++ Scripts/Core/Converters/Converters/Primary/NullableConverter.cs
@@ -0,0 +1,22 @@
+﻿using Rusty.Serialization.Core.Nodes;
+
+namespace Rusty.Serialization.Core.Converters
+{
+    /// <summary>
+    /// A nullable type converter.
+    /// </summary>
+    public sealed class NullableConverter<ValueT> : Converter<ValueT?, INode>
+        where ValueT : struct
+    {
+        /* Protected methods. */
+        protected override INode CreateNode(ValueT? obj, CreateNodeContext context)
+        {
+            return context.CreateNode(obj);
+        }
+
+        protected override ValueT? CreateObject(INode node, CreateObjectContext context)
+        {
+            return (ValueT?)context.CreateObject(typeof(ValueT), node);
+        }
+    }
+}
