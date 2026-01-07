9384d62f5677a646c9a333551b98f7778f1c817e
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2026-01-07 19:54:54 +0100

    Working on JSON serializer.

---
+++ Scripts/JSON/Utilities/ParseUtility.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rusty.Serialization.JSON
+{
+    /// <summary>
+    /// An utility for parsing JSON strings.
+    /// </summary>
+    internal static class ParseUtility
+    {
+        /* Public methods. */
+        /// <summary>
+        /// Takes a delimited string and parses it into a list of strings.
+        /// </summary>
+        public static List<string> Split(string text, char separator = ',')
+        {
+            // Handle null.
+            if (text == null)
+                throw new ArgumentException("Cannot parse null.");
+
+            // Remove trailing whitespace.
+            text = text.Trim();
+
+            // Handle empty list.
+            if (text.Length == 0)
+                return new();
+
+            // Parse text.
+            List<string> result = new();
+            bool inString = false;
+            int depth = 0;
+            int start = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+
+                if (c == '"')
+                {
+                    if (inString)
+                    {
+                        bool isDoubled = i + 1 < text.Length && text[i + 1] == '"';
+                        if (isDoubled)
+                            i++;
+                        else
+                            inString = false;
+                    }
+                    else
+                        inString = true;
+                }
+
+                else if (c == separator && depth == 0)
+                {
+                    string segment = text.Substring(start, i - start).Trim();
+                    result.Add(segment);
+                    start = i + 1;
+                }
+
+                else
+                {
+                    switch (c)
+                    {
+                        case '[':
+                        case '{':
+                            depth++;
+                            break;
+                        case ']':
+                        case '}':
+                            depth--;
+                            break;
+                    }
+                }
+            }
+
+            string final = text.Substring(start).Trim();
+            result.Add(final);
+
+            return result;
+        }
+    }
+}
