fd4be12093faaf82a4a183c9f14b926d26cc4818
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2026-03-20 14:27:02 +0100

    Added address collection method to node tree class.

--- Scripts/Core/Nodes/NodeTree.cs
+++ Scripts/Core/Nodes/NodeTree.cs
@@ -41,5 +41,54 @@ namespace Rusty.Serialization.Core.Nodes
             node.Parent = this;
             Root = node;
         }
+
+        /// <summary>
+        /// Get a lookup of all address nodes in this tree.
+        /// </summary>
+        public Dictionary<string, AddressNode> GetAddressNodes()
+        {
+            if (AddressLookup == null)
+            {
+                AddressLookup = new Dictionary<string, AddressNode>();
+                CollectReferences(Root);
+            }
+            return AddressLookup;
+        }
+
+        /* Private methods. */
+        private void CollectReferences(INode node)
+        {
+            if (node is AddressNode address)
+                AddressLookup[address.Name] = address;
+
+            if (node is IMetadataNode metadata)
+                CollectReferences(metadata);
+            else if (node is ListNode list)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    CollectReferences(list.GetValueAt(i));
+                }
+            }
+            else if (node is DictNode dict)
+            {
+                for (int i = 0; i < dict.Count; i++)
+                {
+                    CollectReferences(dict.GetKeyAt(i));
+                    CollectReferences(dict.GetValueAt(i));
+                }
+            }
+            else if (node is ObjectNode obj)
+            {
+                for (int i = 0; i < obj.Count; i++)
+                {
+                    CollectReferences(obj.GetValueAt(i));
+                }
+            }
+            else if (node is CallableNode func)
+            {
+                CollectReferences(func.Target);
+            }
+        }
     }
 }
